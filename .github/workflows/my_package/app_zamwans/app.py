1c1fd9017d61ab0e72b583ca53a92db2a8cc8bf0
Derek Mwanza <derek.mwanza@navicogroup.com>
2026-01-15 09:59:34 +0000

    add python dependenciesw

---
+++ .github/workflows/my_package/app_zamwans/app.py
@@ -0,0 +1,5 @@
+def hello_world():
+    print("Hello World")
+
+def add(a: int, b: int):
+    return a + b
---
+++ .github/workflows/my_package/tests/test_add.py
@@ -0,0 +1,4 @@
+from app import app_zamwans
+
+def test_add():
+    assert app.add(1,1) == 2
